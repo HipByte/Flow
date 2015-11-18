cfe7a57516cdb5656c62e74e7952dca4f28dea03
Laurent Sansonetti <lrz@hipbyte.com>
2015-11-18 22:50:25 +0100

    template libs

---
+++ lib/android.rb
@@ -0,0 +1,12 @@
+$:.unshift("/Library/RubyMotion/lib")
+require 'motion/project/template/android'
+
+Motion::Project::App.setup do |app|
+  app.api_version = '16' unless Motion::Project::Config.starter?
+  app.build_dir = 'build/android'
+
+  libdir = File.join(File.dirname(__FILE__), '../flow')
+  app.files.concat(Dir.glob(File.join(libdir, '*.rb')))
+  app.files.concat(Dir.glob(File.join(libdir, 'android/*.rb')))
+  app.files.delete_if { |path| path.start_with?('./app/ios') }
+end
---
+++ lib/ios.rb
@@ -0,0 +1,12 @@
+$:.unshift("/Library/RubyMotion/lib")
+require 'motion/project/template/ios'
+
+Motion::Project::App.setup do |app|
+  app.build_dir = 'build/ios'
+  app.deployment_target = '7.0' unless Motion::Project::Config.starter?
+
+  libdir = File.join(File.dirname(__FILE__), '../flow')
+  app.files.concat(Dir.glob(File.join(libdir, '*.rb')))
+  app.files.concat(Dir.glob(File.join(libdir, 'ios/*.rb')))
+  app.files.delete_if { |path| path.start_with?('./app/android') }
+end
