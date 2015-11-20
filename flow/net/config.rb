3f5f7c3343fd2464d0657cc28be9d34906b2f32d
Joffrey Jaffeux <j.jaffeux@gmail.com>
2015-11-20 22:31:02 +0100

    tests for Net::Config

--- flow/net/config.rb
+++ flow/net/config.rb
@@ -2,20 +2,19 @@ module Net
   module Config
     class << self
       attr_accessor :user_agent
-      attr_accessor :verbose
       attr_accessor :connect_timeout
       attr_accessor :read_timeout
 
+      def user_agent
+        @user_agent ||= Net::USER_AGENT
+      end
+
       def connect_timeout
-        @connect_timeout || 30
+        @connect_timeout ||= 30
       end
 
       def read_timeout
-        @read_timeout || 60
-      end
-
-      def verbose
-        @verbose || false
+        @read_timeout ||= 60
       end
     end
   end
---
+++ test/spec/net/config_spec.rb
@@ -0,0 +1,31 @@
+describe "Net::Config" do
+  it "can set connect_timeout" do
+    Net::Config.connect_timeout = 10
+    Net::Config.connect_timeout.should == 10
+  end
+
+  it "has a default connect_timeout" do
+    Net::Config.connect_timeout = nil
+    Net::Config.connect_timeout.should == 30
+  end
+
+  it "can set read_timeout" do
+    Net::Config.read_timeout = 10
+    Net::Config.read_timeout.should == 10
+  end
+
+  it "has a default read_timeout" do
+    Net::Config.read_timeout = nil
+    Net::Config.read_timeout.should == 60
+  end
+
+  it "can set user_agent" do
+    Net::Config.user_agent = "Some User Agent"
+    Net::Config.user_agent.should == "Some User Agent"
+  end
+
+  it "has a default user_agent" do
+    Net::Config.user_agent = nil
+    Net::Config.user_agent.should == Net::USER_AGENT
+  end
+end
