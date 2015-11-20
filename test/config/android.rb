1bb6712d0ce3cef383a1e25dbeeb6ff746555aa9
Laurent Sansonetti <lrz@hipbyte.com>
2015-11-20 16:48:34 +0100

    add test suite (empty for now)

---
+++ test/config/android.rb
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+Motion::Project::App.setup do |app|
+  # Use `rake android:config' to see complete project settings.
+  app.name = 'test'
+  app.archs = ['x86']
+end
---
+++ test/spec/json_spec.rb
@@ -0,0 +1,5 @@
+describe "my awesome spec" do
+  it "works" do
+    1.should == 1
+  end
+end
