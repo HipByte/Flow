9f99b6c97a46a24d29985bad76e850cbefd40149
Laurent Sansonetti <lrz@hipbyte.com>
2015-11-20 14:55:42 +0100

    basic implementation for json/ios

--- flow/json/ios/json.rb
+++ flow/json/ios/json.rb
@@ -1,11 +1,22 @@
 class JSON
   def self.load(str)
-    nil
+    error_ptr = Pointer.new(:id)
+    obj = NSJSONSerialization.JSONObjectWithData(str.to_data, options:0, error:error_ptr)
+    if obj == nil
+      raise error_ptr[0].description
+    end
+    obj
   end
 end
 
 class Object
   def to_json
-    nil
+    raise "Invalid JSON object" unless NSJSONSerialization.isValidJSONObject(self)
+    error_ptr = Pointer.new(:id)
+    data = NSJSONSerialization.dataWithJSONObject(self, options:0, error:error_ptr)
+    if data == nil
+      raise error_ptr[0].description
+    end
+    data.to_str
   end
 end
