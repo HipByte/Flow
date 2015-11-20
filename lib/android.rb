9f2d00d1e352c77a9aa1ed1d0b6e057092aa9edf
Joffrey Jaffeux <j.jaffeux@gmail.com>
2015-11-20 17:15:01 +0100

    load store lib

--- lib/android.rb
+++ lib/android.rb
@@ -5,7 +5,7 @@ Motion::Project::App.setup do |app|
   app.api_version = '16' unless Motion::Project::Config.starter?
   app.build_dir = 'build/android'
 
-  libdir = File.join(File.dirname(__FILE__), '../flow/net')
+  libdir = File.join(File.dirname(__FILE__), '../flow/*')
   app.files.concat(Dir.glob(File.join(libdir, '*.rb')))
   app.files.concat(Dir.glob(File.join(libdir, 'android/*.rb')))
   app.files.delete_if { |path| path.start_with?('./app/ios') }
