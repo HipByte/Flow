6a54149a49f9c369f1e5380f86de9d6f2e348269
Laurent Sansonetti <lrz@hipbyte.com>
2015-11-22 13:12:25 +0100

    force api version 22

--- test/config/android.rb
+++ test/config/android.rb
@@ -4,6 +4,7 @@ Motion::Project::App.setup do |app|
   # Use `rake android:config' to see complete project settings.
   app.name = 'test'
   app.archs = ['x86']
+  app.api_version = '22'
   app.vm_debug_logs = true
   app.permissions = ["android.permission.INTERNET"]
 end
