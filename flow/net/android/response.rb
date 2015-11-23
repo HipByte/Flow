ece1954df2c8343499432c894459d05a09a87be9
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2015-11-23 13:54:44 +0100

    Support for mime_type and json on android

--- flow/net/android/response.rb
+++ flow/net/android/response.rb
@@ -10,13 +10,30 @@ module Net
     end
 
     def body
-      @response.toString
+      @body ||= build_body
     end
 
     def status_message
     end
 
     def headers
+    end
+
+    def json?
+      mime_type.match /application\/json/
+    end
+
+    def mime_type
+      @connection.getContentType
+    end
+
+    private
+
+    def build_body
+      if json?
+        return JSON.load(@response.toString)
+      end
+      @response.toString
     end
   end
 end
