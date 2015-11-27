1f053779795b2d28a39bf42ee3cf322fa4c08c56
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2015-11-27 23:47:31 +0100

    Refactoring actions with define_method

--- flow/net/actions.rb
+++ flow/net/actions.rb
@@ -1,39 +1,12 @@
 module Net
   class Request
     module Actions
-      def get(base_url, options, callback)
-        request = Request.new(base_url, options.merge(method: :get), callback)
-        request.run
-      end
-
-      def post(base_url, options, callback)
-        request = Request.new(base_url, options.merge(method: :post), callback)
-        request.run
-      end
-
-      def put(base_url, options, callback)
-        request = Request.new(base_url, options.merge(method: :put), callback)
-        request.run
-      end
-
-      def delete(base_url, options, callback)
-        request = Request.new(base_url, options.merge(method: :delete), callback)
-        request.run
-      end
-
-      def head(base_url, options, callback)
-        request = Request.new(base_url, options.merge(method: :head), callback)
-        request.run
-      end
-
-      def patch(base_url, options, callback)
-        request = Request.new(base_url, options.merge(method: :patch), callback)
-        request.run
-      end
-
-      def options(base_url, options, callback)
-        request = Request.new(base_url, options.merge(method: :options), callback)
-        request.run
+      [:get, :post, :put, :delete, :patch, :options, :head].each do |http_medhod|
+        define_method(http_medhod) do |base_url, *options, &callback|
+          options = options.shift || {}
+          request = Request.new(base_url, options.merge(method: http_medhod))
+          request.run(&callback)
+        end
       end
     end
   end
