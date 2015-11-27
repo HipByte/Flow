461f9517fc773f72d2e0fe5c5f9949e1b7dcc074
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2015-11-27 23:57:08 +0100

    Move method in class methods

--- flow/net/net.rb
+++ flow/net/net.rb
@@ -1,11 +1,11 @@
 module Net
   USER_AGENT = "Net - https://bitbucket.org/hipbyte/flow"
 
-  def self.session(base_url, &block)
-    Session.build(base_url, &block)
-  end
-
   class << self
+    def session(base_url, &block)
+      Session.build(base_url, &block)
+    end
+
     [:get, :post, :put, :delete, :patch, :options, :head].each do |http_medhod|
       define_method(http_medhod) do |base_url, *options, &callback|
         Request.send(http_medhod, base_url, options.shift || {}, &callback)
