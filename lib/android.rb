77d900c8872c7b2c42fbefd1bb43ca2106f35652
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2015-11-29 16:25:51 +0100

    Revert "Introduce shared folder architecture"
    This reverts commit 1998dcb61bcf40bc3383770fb73429d3c30b563b.

--- lib/android.rb
+++ lib/android.rb
@@ -11,9 +11,9 @@ Motion::Project::App.setup do |app|
   FLOW_COMPONENTS.each do |comp|
     libdir = File.join(File.dirname(__FILE__), '../flow/' + comp)
     app.files.concat(Dir.glob(File.join(libdir, '*.rb')))
-    app.files.concat(Dir.glob(File.join(libdir, 'shared/**/*.rb')))
-    app.files.concat(Dir.glob(File.join(libdir, 'android/*.rb')))
+    app.files.concat(Dir.glob(File.join(libdir, '**/*.rb')))
     app.files.delete_if { |path| path.include?('ios') }
   end
+  app.files.delete_if { |path| path.start_with?('./app/ios') }
   app.spec_files.delete_if { |path| path.start_with?('./spec/helpers/ios') }
 end
