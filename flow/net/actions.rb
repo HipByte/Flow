6577a3f9d32b2d4285a568dc11634854408bd132
Laurent Sansonetti <lrz@hipbyte.com>
2015-12-03 14:59:05 +0100

    typo

--- flow/net/actions.rb
+++ flow/net/actions.rb
@@ -1,10 +1,10 @@
 module Net
   class Request
     module Actions
-      [:get, :post, :put, :delete, :patch, :options, :head].each do |http_medhod|
-        define_method(http_medhod) do |base_url, *options, callback|
+      [:get, :post, :put, :delete, :patch, :options, :head].each do |http_method|
+        define_method(http_method) do |base_url, *options, callback|
           options = options.shift || {}
-          request = Request.new(base_url, options.merge(method: http_medhod))
+          request = Request.new(base_url, options.merge(method: http_method))
           request.run(&callback)
         end
       end
