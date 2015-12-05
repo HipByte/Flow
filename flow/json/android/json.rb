2369067e7510432ff698470bb740b59af83a9224
Laurent Sansonetti <lrz@hipbyte.com>
2015-12-05 14:52:45 +0100

    fix decoding of nested json objects, fix #to_json to return a ruby string, reorganize the code better with private lambdas

--- flow/json/android/json.rb
+++ flow/json/android/json.rb
@@ -1,35 +1,3 @@
-class Org::JSON::JSONArray
-  def _to_obj
-    ary = Array.new
-    length.times do |i|
-      obj = get(i)
-      case obj
-        when Org::JSON::JSONObject, Org::JSON::JSONArray
-          obj = obj._to_obj
-      end
-      ary << obj
-    end
-    ary
-  end
-end
-
-class Org::JSON::JSONObject
-  def _to_obj
-    iter = self.keys
-    hash = Hash.new
-    while iter.hasNext
-      key = iter.next
-      obj = get(key)
-      case obj
-        when Org::JSON::JSONObject, Org::JSON::JSONArray
-          obj = obj._to_obj
-      end
-      hash[key.to_s] = obj
-    end
-    hash
-  end
-end
-
 class JSON
   def self.load(str)
     tok = Org::JSON::JSONTokener.new(str)
@@ -37,37 +5,58 @@ class JSON
     if obj == nil
       raise "Can't deserialize object from JSON"
     end
-    case obj
-      when Org::JSON::JSONObject, Org::JSON::JSONArray
-        obj = obj._to_obj
-    end
-    obj
+
+    # Transform pure-Java JSON objects to Ruby types.
+    @convert_java ||= (lambda do |obj|
+      case obj
+        when Org::JSON::JSONArray
+          obj.length.times.map { |i| @convert_java.call(obj.get(i)) }
+        when Org::JSON::JSONObject
+          iter = obj.keys
+          hash = Hash.new
+          while iter.hasNext
+            key = iter.next
+            value = obj.get(key)
+            hash[@convert_java.call(key)] = @convert_java.call(value)
+          end
+          hash
+        when Java::Lang::String
+          obj.to_s
+        else
+          obj
+      end
+    end)
+    @convert_java.call(obj)
   end
 end
 
 class Object
   def to_json
     # The Android JSON API expects real Java String objects.
-    obj = self
-    case obj
-      when String
-        obj = obj.toString
-      when Hash
-        map = Hash.new
-        obj.each do |key, value|
-          key = key.toString if key.is_a?(String)
-          value = value.toString if value.is_a?(String)
-          map[key] = value
-        end
-        obj = map
-      when Array
-        obj = obj.map { |item| item.is_a?(String) ? item.toString : item }
-    end
+    @@fix_string ||= (lambda do |obj|
+      case obj
+        when String
+          obj = obj.toString
+        when Hash
+          map = Hash.new
+          obj.each do |key, value|
+            key = key.toString if key.is_a?(String)
+            value = @@fix_string.call(value)
+            map[key] = value
+          end
+          obj = map
+        when Array
+          obj = obj.map do |item|
+            item.is_a?(String) ? item.toString : @@fix_string.call(item)
+          end
+      end
+      obj
+    end)
 
-    obj = Org::JSON::JSONObject.wrap(obj)
+    obj = Org::JSON::JSONObject.wrap(@@fix_string.call(self))
     if obj == nil
       raise "Can't serialize object to JSON"
     end
-    obj.toString
+    obj.toString.to_s
   end
 end
