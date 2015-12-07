21f1103fc843ba255beaff61b6a8d47b11723db2
Joffrey Jaffeux <j.jaffeux@gmail.com>
2015-12-07 21:45:53 +0100

    encode and decode are class methods

--- flow/base64/android/base64.rb
+++ flow/base64/android/base64.rb
@@ -1,11 +1,11 @@
 module Flow
   class Base64
-    def encode(string)
+    def self.encode(string)
       bytes = string.getBytes("UTF-8")
       Android::Util::Base64.encodeToString(bytes, Android::Util::Base64::DEFAULT)
     end
 
-    def decode(string)
+    def self.decode(string)
       bytes = Android::Util::Base64.decode(string, Android::Util::Base64::DEFAULT)
       Java::Lang::String.new(bytes, "UTF-8")
     end
