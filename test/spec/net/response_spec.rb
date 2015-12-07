acd641407fc9c9321fdc749043290f9ffcd89b68
Joffrey Jaffeux <j.jaffeux@gmail.com>
2015-12-07 20:25:56 +0100

    homogeneize coding style

--- test/spec/net/response_spec.rb
+++ test/spec/net/response_spec.rb
@@ -1,6 +1,6 @@
 describe Net::Response do
   before do
-    @response = Net::Response.new({
+    @subject = Net::Response.new({
       status_code: 200,
       status_message: :ok,
       mime_type: "application/json",
@@ -8,19 +8,19 @@ describe Net::Response do
     })
   end
 
-  it "has a status" do
-    @response.status.should == 200
+  it ".status" do
+    @subject.status.should == 200
   end
 
-  it "has a status_message" do
-    @response.status_message.should == :ok
+  it ".status_message" do
+    @subject.status_message.should == :ok
   end
 
-  it "has a mime_type" do
-    @response.mime_type.should == "application/json"
+  it ".mime_type" do
+    @subject.mime_type.should == "application/json"
   end
 
-  it "has a body" do
-    @response.body.should == ""
+  it ".body" do
+    @subject.body.should == ""
   end
 end
