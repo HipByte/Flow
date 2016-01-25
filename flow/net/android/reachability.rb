2af401be47f39810c8e5ed5595d6fcf482bbc76f
Laurent Sansonetti <lrz@hipbyte.com>
2016-01-25 17:28:05 +0100

    android implementation for Net.reachable?

--- flow/net/android/reachability.rb
+++ flow/net/android/reachability.rb
@@ -1,7 +1,54 @@
+class ReachabilityDelegate < Android::Net::ConnectivityManager::NetworkCallback
+  def initialize(hostname, block)
+    @hostname = hostname
+    @block = block
+  end
+
+  def onAvailable(network)
+    begin
+      sockaddr = Java::Net::InetSocketAddress.new(@hostname, 80)
+      sock = Java::Net::Socket.new
+      sock.connect(sockaddr, 2000) # 2 seconds timeout
+      sock.close
+      _available(true)
+    rescue
+      _available(false)
+    end
+  end
+
+  def onLost(network)
+    _available(false)
+  end
+
+  def _available(flag)
+    @block.call(flag)
+  end
+end
+
 module Net
+  def self.context=(context)
+    @connectivity = context.getSystemService(Android::Content::Context::CONNECTIVITY_SERVICE)
+  end
+
+  def self._connectivity
+    @connectivity or raise "Call `Net.context = self' in your main activity"
+  end
+
   class Reachability
     def initialize(hostname, &block)
-      raise "Not Implemented"
+      builder = Android::Net::NetworkRequest::Builder.new
+      builder.addCapability(Android::Net::NetworkCapabilities::NET_CAPABILITY_INTERNET)
+      builder.addTransportType(Android::Net::NetworkCapabilities::TRANSPORT_WIFI)
+      builder.addTransportType(Android::Net::NetworkCapabilities::TRANSPORT_CELLULAR)
+      @delegate = ReachabilityDelegate.new(hostname, block)
+      Net._connectivity.registerNetworkCallback(builder.build, @delegate) 
+    end
+
+    def stop
+      if @delegate
+        Net._connectivity.unregisterNetworkCallback(@delegate)
+        @delegate = nil
+      end
     end
   end
 end
