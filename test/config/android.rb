a27ee8450d93e6dfb0101ed9cbb810c6f97f91d8
Laurent Sansonetti <lrz@hipbyte.com>
2016-01-29 18:54:45 +0100

    add location permission in order to test the location api

--- test/config/android.rb
+++ test/config/android.rb
@@ -3,8 +3,8 @@
 Motion::Project::App.setup do |app|
   # Use `rake android:config' to see complete project settings.
   app.name = 'test'
-  app.archs = ['x86']
+  #app.archs = ['x86']
   app.api_version = '22'
   app.vm_debug_logs = true
-  app.permissions = ["android.permission.INTERNET", "android.permission.ACCESS_NETWORK_STATE"]
+  app.permissions = ["android.permission.INTERNET", "android.permission.ACCESS_NETWORK_STATE", "android.permission.ACCESS_FINE_LOCATION"]
 end
