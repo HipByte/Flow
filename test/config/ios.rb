a1aa7bcbf64f40bd319b0d5d88fbf4595cc2bab0
Laurent Sansonetti <lrz@hipbyte.com>
2016-01-29 15:51:25 +0100

    info.plist entries to test core location

--- test/config/ios.rb
+++ test/config/ios.rb
@@ -4,4 +4,6 @@ Motion::Project::App.setup do |app|
   # Use `rake ios:config' to see complete project settings.
   app.name = 'test'
   app.info_plist['NSAppTransportSecurity'] = { 'NSAllowsArbitraryLoads' => true }
+  app.info_plist['NSLocationWhenInUseUsageDescription'] = "Required to test location services"
+  app.info_plist['NSLocationAlwaysUsageDescription'] = "Required to test location services"
 end
