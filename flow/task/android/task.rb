080018ff2808b026cf0d346c38b215049f991d27
Laurent Sansonetti <lrz@hipbyte.com>
2016-02-02 14:20:44 +0100

    android implementation for Task

---
+++ flow/task/android/task.rb
@@ -0,0 +1,39 @@
+class Task
+  class Timer
+    def initialize(interval, repeats, block)
+      interval *= 1000
+      if interval <= 0
+        raise ArgError, "negative or too small interval"
+      end
+      @timer = Java::Util::Concurrent::Executors.newSingleThreadScheduledExecutor
+      @future =
+        if repeats
+          @timer.scheduleAtFixedRate(block, interval, interval, Java::Util::Concurrent::TimeUnit::MILLISECONDS)
+        else
+          @timer.schedule(block, interval, Java::Util::Concurrent::TimeUnit::MILLISECONDS)
+        end
+    end
+
+    def stop
+      if @future
+        @future.cancel(true)
+        @future = nil
+      end
+    end
+  end
+
+  class Queue
+    def self.schedule_on_main(block)
+      @main_handle ||= begin
+        looper = Android::Os::Looper.getMainLooper
+        Android::Os::Handler.new(Android::Os::Looper.getMainLooper)
+      end
+      @main_handle.post(block)
+    end
+
+    def self.schedule_on_background(block)
+      @thread_pool ||= Java::Util::Concurrent::Executors.newCachedThreadPool
+      @thread_pool.execute(block)
+    end
+  end
+end
