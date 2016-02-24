f8add0173e813fb16a14706fcd04ade953519d35
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2016-02-24 17:06:10 +0100

    Better sample

--- ui_app/app/welcome_screen.rb
+++ ui_app/app/welcome_screen.rb
@@ -3,14 +3,21 @@ class WelcomeScreen < UI::Screen
 
   def on_load
     background = UI::View.new
-    background.width = 300
-    background.height = 400
+    background.flex = 1
+    background.margin_top = 25
+    background.margin_left = 25
+    background.margin_right = 25
+    background.margin_bottom = 25
     background.background_color = :blue
     self.view.add_child(background)
 
     label = UI::Label.new
-    label.width = 230
-    label.height = 30
+    label.height = 50
+    label.margin_top = 5
+    label.margin_left = 5
+    label.margin_right = 5
+    label.margin_bottom = 5
+    label.flex_direction = :column
     label.text = "Hello world"
     label.background_color = :red
     label.color = :white
@@ -18,16 +25,18 @@ class WelcomeScreen < UI::Screen
     background.add_child(label)
 
     button = UI::Button.new
-    button.width = 230
-    button.height = 20
+    button.height = 50
+    button.margin_left = 5
+    button.margin_right = 5
+    button.flex_direction = :column
     button.title = "Submit"
-    button.background_color =:yellow
+    button.background_color = :yellow
     button.color = :black
     button.border_width = 2.0
     button.border_color = :black
     button.border_radius = 3.0
     background.add_child(button)
 
-    background.layout!
+    self.view.layout!
   end
 end
