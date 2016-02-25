76b84fa67dec3260da717696db60204c5bf7a2e0
Laurent Sansonetti <lrz@hipbyte.com>
2016-02-25 16:35:22 +0100

    fix android resources

--- lib/android.rb
+++ lib/android.rb
@@ -6,6 +6,8 @@ require 'motion/project/template/android'
 Motion::Project::App.setup do |app|
   app.api_version = '21' unless Motion::Project::Config.starter?
   app.build_dir = 'build/android'
+  app.assets_dirs = ['resources']
+  app.resources_dirs = []
 
   FLOW_COMPONENTS.each do |comp|
     libdir = File.join(File.dirname(__FILE__), '../flow/' + comp)
