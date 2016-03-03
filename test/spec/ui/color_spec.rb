a92756836e07fae9c6eea2fa8ac6cef5f39e1b15
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2016-03-03 17:06:38 +0100

    UI::Color spec is now cross platform
    Android spec is failing due to what is probably a rounding issue.

--- test/spec/ui/color_spec.rb
+++ test/spec/ui/color_spec.rb
@@ -1,48 +1,39 @@
 describe UI::Color do
   context "#() protocol conversion" do
-    before do
-      @blue_color = UIColor.blueColor
-      @blue_alpha_color = UIColor.colorWithRed(0/255.0, green:0/255.0, blue:1/255.0, alpha:1.0)
-
-      r, g, b, a = [1, (0x8A.to_f/0xFF.to_f), (0x19/0xFF.to_f), (0x88.to_f/0xFF.to_f)]
-      @orange_color = UIColor.colorWithRed(r, green:g, blue:b, alpha:1.0)
-      @orange_alpha_color = UIColor.colorWithRed(r, green:g, blue:b, alpha:a)
-    end
-
     it "should convert a String with a #" do
-      UI::Color("#00F").container.should == @blue_color
+      UI::Color("#00F").to_a.should == [0, 0, 255, 255]
     end
 
     it "should convert a String without a #" do
-      UI::Color("00F").container.should == @blue_color
-    end
-
-    it "should convert a String without 3 digits" do
-      UI::Color("00F").container.should == @blue_color
+      UI::Color("00F").to_a.should == [0, 0, 255, 255]
     end
 
     it "should convert a String without 6 digits" do
-      UI::Color("FF8A19").container.should == @orange_color
+      UI::Color("FF8A19").to_a.should == [255, 137, 24, 255]
     end
 
     it "should convert a String without 8 digits (alpha component)" do
-      UI::Color("88FF8A19").container.should == @orange_alpha_color
+      UI::Color("88FF8A19").to_a.should == [255, 137, 24, 135]
     end
 
     it "should convert a 3 values Array to rgba with default alpha = 255" do
-      UI::Color([0, 0, 1]).container.should == @blue_alpha_color
+      UI::Color([0, 0, 1]).to_a.should == [0, 0, 1, 255]
     end
 
     it "should convert a 4 values Array" do
-      UI::Color([0, 0, 1, 255]).container.should == @blue_alpha_color
+      UI::Color([0, 0, 1, 255]).to_a.should == [0, 0, 1, 255]
     end
 
-    it "should convert a UIColor" do
-      UI::Color(UIColor.blueColor).container.should == @blue_color
+    it "should convert native platform Color object" do
+      if defined?(UIColor)
+        UI::Color(UIColor.blueColor).to_a.should == [0, 0, 255, 255]
+      else
+        UI::Color(Android::Graphics::Color.argb(255, 0, 0, 255)).to_a.should == [0, 0, 255, 255]
+      end
     end
 
     it "should convert a Symbol" do
-      UI::Color(:blue).container.should == UIColor.blueColor
+      UI::Color(:blue).to_a.should == [0, 0, 255, 255]
     end
   end
 end
