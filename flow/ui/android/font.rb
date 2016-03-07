b991fbc5a0524b933e8b31aec16a51afd74bf818
Laurent Sansonetti <lrz@hipbyte.com>
2016-03-07 15:32:34 +0100

    create fonts with names and not family types, handle builtin fonts and retrieve custom (assets) fonts if needed

--- flow/ui/android/font.rb
+++ flow/ui/android/font.rb
@@ -4,21 +4,42 @@ module UI
       new(font, size, nil)
     end
 
+    BuiltinFonts = {
+      'ComingSoon' => 'casual',
+      'DancingScript-Regular' => 'cursive',
+      'DroidSansMono' => 'monospace',
+      'Roboto-Regular' => 'sans-serif',
+      'Roboto-Black' => 'sans-serif-black',
+      'RobotoCondensed-Regular' => 'sans-serif-condensed',
+      'RobotoCondensed-Light' => 'sans-serif-condensed-light',
+      'Roboto-Light' => 'sans-serif-light',
+      'Roboto-Medium' => 'sans-serif-medium',
+      'CarroisGothicSC-Regular' => 'sans-serif-smallcaps',
+      'Roboto-Thin' => 'sans-serif-thin',
+      'NotoSerif-Regular' => 'serif',
+      'CutiveMono' => 'serif-monospace'
+    }
+
     def initialize(obj, size, trait=nil)
       if obj.is_a?(Android::Graphics::Typeface)
         @container = obj
       else
-        style = case trait
-          when :bold
-            Android::Graphics::Typeface::BOLD
-          when :italic
-            Android::Graphics::Typeface::ITALIC
-          when :bold_italic
-            Android::Graphics::Typeface::BOLD_ITALIC
-          else
-            Android::Graphics::Typeface::NORMAL
+        family_name = BuiltinFonts[obj]
+        if family_name
+          style = case trait
+            when :bold
+              Android::Graphics::Typeface::BOLD
+            when :italic
+              Android::Graphics::Typeface::ITALIC
+            when :bold_italic
+              Android::Graphics::Typeface::BOLD_ITALIC
+            else
+              Android::Graphics::Typeface::NORMAL
+          end
+          @container = Android::Graphics::Typeface.create(family_name, style)
+        else
+          @container = Android::Graphics::Typeface.createFromAsset(UI.context.getAssets, obj + '.ttf')
         end
-        @container = Android::Graphics::Typeface.create(obj, style)
       end
       @size = size
     end
