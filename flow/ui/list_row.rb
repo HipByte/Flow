557c1c48e8063d580fc5ff3c30dc64aa9c2d399e
Laurent Sansonetti <lrz@hipbyte.com>
2016-03-09 17:25:53 +0100

    some tweaks

--- flow/ui/list_row.rb
+++ flow/ui/list_row.rb
@@ -6,7 +6,8 @@ module UI
 
       @label = UI::Label.new
       @label.flex = 1
-      @label.margin = 10
+      @label.margin = [10, 15]
+      @label.padding = 5
       self.add_child(@label)
     end
 
