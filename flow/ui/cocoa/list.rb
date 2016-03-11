dad3906562605cd9dff903930f6b52af64d2d904
Laurent Sansonetti <lrz@hipbyte.com>
2016-03-11 19:13:36 +0100

    support for multiple row classes, properly re-layout view cells when the table view resizes them

--- flow/ui/cocoa/list.rb
+++ flow/ui/cocoa/list.rb
@@ -3,12 +3,18 @@ module UI
     class CustomListCell < UITableViewCell
       IDENTIFIER = "CustomListCell"
 
-      attr_accessor :content_view
+      attr_reader :content_view
 
       def content_view=(content_view)
         @content_view = content_view
         self.contentView.addSubview(@content_view.container)
-        @content_view.width = self.contentView.frame.size.width
+        @content_view.width = contentView.frame.size.width
+      end
+
+      def layoutSubviews
+        @content_view.width = contentView.frame.size.width
+        @content_view.update_layout
+        super
       end
     end
 
@@ -16,8 +22,6 @@ module UI
       super
       @data_source = []
       @render_row_block = lambda { |section_index, row_index| ListRow }
-      container.registerClass(CustomListCell, forCellReuseIdentifier: CustomListCell::IDENTIFIER)
-      @sizing_cell = CustomListCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: CustomListCell::IDENTIFIER)
     end
 
     def numberOfSectionsInTableView(table_view)
@@ -25,35 +29,34 @@ module UI
     end
 
     def tableView(table_view, numberOfRowsInSection: section)
-      @data_source.count
-    end
-
-    def tableView(table_view, heightForRowAtIndexPath: index_path)
-      if !@sizing_cell.content_view
-        content_view = @render_row_block.call(index_path.section, index_path.row).new
-        @sizing_cell.content_view = content_view
-      end
-      @sizing_cell.content_view.update(@data_source[index_path.row])
-      @sizing_cell.content_view.update_layout
-      @sizing_cell.content_view.layout[3]
+      @data_source.size
     end
 
     def tableView(table_view, cellForRowAtIndexPath: index_path)
-      cell = table_view.dequeueReusableCellWithIdentifier(CustomListCell::IDENTIFIER, forIndexPath: index_path)
-      if !cell.content_view
-        content_view = @render_row_block.call(index_path.section, index_path.row).new
-        cell.content_view = content_view
+      row_klass = @render_row_block.call(index_path.section, index_path.row)
+      cell_identifier = CustomListCell::IDENTIFIER + row_klass.name
+      cell = table_view.dequeueReusableCellWithIdentifier(cell_identifier)
+      unless cell
+        cell = CustomListCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: cell_identifier)
+        cell.content_view = row_klass.new
       end
       cell.content_view.update(@data_source[index_path.row])
       cell.content_view.update_layout
       cell
     end
 
+    def tableView(table_view, heightForRowAtIndexPath: index_path)
+      cell = tableView(table_view, cellForRowAtIndexPath: index_path)
+      cell.content_view.layout[3]
+    end
+
     attr_reader :data_source
 
     def data_source=(data_source)
-      @data_source = data_source
-      container.reloadData
+      if @data_source != data_source
+        @data_source = data_source
+        container.reloadData
+      end
     end
 
     def render_row(&block)
