749136d88e2a7f2de89c8e84524357fe58f415fd
Laurent Sansonetti <lrz@hipbyte.com>
2016-03-16 17:44:29 +0530

    misc cleanup

--- flow/ui/android/screen.rb
+++ flow/ui/android/screen.rb
@@ -45,13 +45,15 @@ module UI
         view.container.setLayoutParams(Android::View::ViewGroup::LayoutParams.new(Android::View::ViewGroup::LayoutParams::MATCH_PARENT, Android::View::ViewGroup::LayoutParams::MATCH_PARENT))
         metrics = Android::Util::DisplayMetrics.new
         main_screen_metrics = UI.context.windowManager.defaultDisplay.getMetrics(metrics)
-        view.width = main_screen_metrics.width / UI.density
-        title_bar_height = 0
+
+        view_height = main_screen_metrics.height
         resource_id = UI.context.resources.getIdentifier("status_bar_height", "dimen", "android")
         if resource_id > 0
-          title_bar_height = UI.context.resources.getDimensionPixelSize(resource_id)
+          view_height -= UI.context.resources.getDimensionPixelSize(resource_id)
         end
-        view.height = (main_screen_metrics.height - title_bar_height) / UI.density
+
+        view.width = main_screen_metrics.width / UI.density
+        view.height = view_height / UI.density
         view
       end
     end
