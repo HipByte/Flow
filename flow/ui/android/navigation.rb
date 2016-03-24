75ef55736db55821c07f55ae4c6c877852ef65d5
Laurent Sansonetti <lrz@hipbyte.com>
2016-03-24 17:28:16 +0100

    fix regression introduced by fef1b29600db1f71e084fdd90948d9bb7b04fd84

--- flow/ui/android/navigation.rb
+++ flow/ui/android/navigation.rb
@@ -61,7 +61,7 @@ class UI::Navigation
   end
 
   def bar_color=(color)
-    UI.context.supportActionBar.backgroundDrawable = Android::Graphics::Drawable::ColorDrawable.new() 
+    UI.context.supportActionBar.backgroundDrawable = Android::Graphics::Drawable::ColorDrawable.new(UI::Color(color).proxy)
   end
 
   def push(screen, animated=true)

HipByte/Flow

commit fef1b29600db1f71e084fdd90948d9bb7b04fd84

Homogeneise naming/formating

flow/ui/android/navigation.rb
@@ -61,7 +61,7 @@ class UI::Navigation
   end
 
   def bar_color=(color)
-    UI.context.supportActionBar.backgroundDrawable = Android::Graphics::Drawable::ColorDrawable.new(UI::Color(color).proxy) 
+    UI.context.supportActionBar.backgroundDrawable = Android::Graphics::Drawable::ColorDrawable.new() 
   end
 
   def push(screen, animated=true)

flow/ui/android/activity_indicator.rb
@@ -19,9 +19,9 @@ module UI
 
     def proxy
       @proxy ||= begin
-        pbar = Android::Widget::ProgressBar.new(UI.context)
-        pbar.setIndeterminate(true)
-        pbar
+        progress_bar = Android::Widget::ProgressBar.new(UI.context)
+        progress_bar.setIndeterminate(true)
+        progress_bar
       end
     end
   end

flow/ui/android/application.rb
@@ -16,9 +16,9 @@ class UI::Application
 
   def proxy
     @proxy ||= begin
-      proxy = Android::Widget::FrameLayout.new(UI.context)
-      proxy.id = Android::View::View.generateViewId
-      proxy
+      frame_layout = Android::Widget::FrameLayout.new(UI.context)
+      frame_layout.id = Android::View::View.generateViewId
+      frame_layout
     end
   end
 end

flow/ui/android/button.rb
@@ -42,7 +42,7 @@ module UI
     end
 
     def image=(image)
-      _change_type :image
+      _change_type(:image)
       stream = UI.context.getAssets.open(image)
       drawable = Android::Graphics::Drawable::Drawable.createFromStream(stream, nil)
       proxy.imageDrawable = drawable

flow/ui/android/label.rb
@@ -9,12 +9,12 @@ module UI
     end
 
     def measure(width, height)
-      dim = [width, height]
+      dimension = [width, height]
       unless width.nan?
         layout = Android::Text::StaticLayout.new(proxy.text, proxy.getPaint, width, Android::Text::Layout::Alignment::ALIGN_NORMAL, 1, 0, true)
-        dim[1] = layout.getHeight
+        dimension[1] = layout.getHeight
       end
-      dim
+      dimension
     end
 
     def proxy

flow/ui/android/list.rb
@@ -67,14 +67,14 @@ module UI
 
     def proxy
       @proxy ||= begin
-        list = Android::Widget::ListView.new(UI.context)
-        list.adapter = FlowUIListViewAdapter.new(self)
-        list.onItemClickListener = FlowUIListItemClickListener.new(self)
-        list.divider = nil
-        list.dividerHeight = 0
-        list.itemsCanFocus = true
-        list.descendantFocusability = Android::View::ViewGroup::FOCUS_AFTER_DESCENDANTS
-        list
+        list_view = Android::Widget::ListView.new(UI.context)
+        list_view.adapter = FlowUIListViewAdapter.new(self)
+        list_view.onItemClickListener = FlowUIListItemClickListener.new(self)
+        list_view.divider = nil
+        list_view.dividerHeight = 0
+        list_view.itemsCanFocus = true
+        list_view.descendantFocusability = Android::View::ViewGroup::FOCUS_AFTER_DESCENDANTS
+        list_view
       end
     end
   end

flow/ui/android/text_input.rb
@@ -31,10 +31,10 @@ module UI
 
     def proxy
       @proxy ||= begin
-        view = Android::Widget::EditText.new(UI.context)
-        view.setPadding(0, 0, 0, 0)
-        view.addTextChangedListener FlowUITextInputTextChangedListener.new(self)
-        view
+        edit_text = Android::Widget::EditText.new(UI.context)
+        edit_text.setPadding(0, 0, 0, 0)
+        edit_text.addTextChangedListener FlowUITextInputTextChangedListener.new(self)
+        edit_text
       end
     end
   end

flow/ui/android/view.rb
@@ -1,4 +1,4 @@
-class FlowViewLayoutChangeListener
+class FlowUIViewLayoutChangeListener
   def initialize(view)
     @view = view
   end
@@ -62,13 +62,13 @@ module UI
         params.height = height
         proxy.layoutParams = params
       end
-      children.each { |x| x._apply_layout }
+      children.each { |child| child._apply_layout }
     end
 
     def _autolayout_when_resized=(value)
       if value
         unless @layout_listener
-          @layout_listener = FlowViewLayoutChangeListener.new(self)
+          @layout_listener = FlowUIViewLayoutChangeListener.new(self)
           proxy.addOnLayoutChangeListener(@layout_listener)
         end
       else

commit 75ef55736db55821c07f55ae4c6c877852ef65d5

fix regression introduced by fef1b29600db1f71e084fdd90948d9bb7b04fd84

flow/ui/android/navigation.rb
@@ -61,7 +61,7 @@ class UI::Navigation
   end
 
   def bar_color=(color)
-    UI.context.supportActionBar.backgroundDrawable = Android::Graphics::Drawable::ColorDrawable.new() 
+    UI.context.supportActionBar.backgroundDrawable = Android::Graphics::Drawable::ColorDrawable.new(UI::Color(color).proxy)
   end
 
   def push(screen, animated=true)