f0d876c53ab6e0fabb4d4222e2dd3f9e868c8b51
Laurent Sansonetti <lrz@hipbyte.com>
2016-03-26 19:05:38 +0100

    style

--- flow/ui/cocoa/screen.rb
+++ flow/ui/cocoa/screen.rb
@@ -7,10 +7,10 @@ module UI
     def initWithScreen(screen)
       if init
         @screen = screen
-        on(:view_did_load) { @screen.before_on_load}
-        on(:view_did_load) { @screen.before_on_load}
-        on(:view_will_appear) { @screen.before_on_show}
-        on(:view_did_appear) { @screen.on_show}
+        on(:view_did_load) { @screen.before_on_load }
+        on(:view_did_load) { @screen.before_on_load }
+        on(:view_will_appear) { @screen.before_on_show }
+        on(:view_did_appear) { @screen.on_show }
       end
       self
     end
