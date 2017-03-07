77df8b70fa4004297d82b5738f0d1dd0bdb93df4
Laurent Sansonetti <lrz@hipbyte.com>
2017-03-07 23:47:38 +0100

    add UI.resource_str (to read a text resource file)

--- flow/ui/android/ui.rb
+++ flow/ui/android/ui.rb
@@ -10,4 +10,18 @@ module UI
   def self.density
     @density ||= UI.context.resources.displayMetrics.density
   end
+
+  def self.resource_str(name)
+    if stream = UI.context.assets.open(name)
+      input_reader = Java::Io::InputStreamReader.new(stream)
+      input = Java::Io::BufferedReader.new(input_reader)
+      buf = Java::Lang::StringBuffer.new
+      loop do
+        break unless line = input.readLine
+        buf.append(line)
+      end
+      input.close
+      buf.toString
+    end
+  end
 end
--- flow/ui/cocoa/ui.rb
+++ flow/ui/cocoa/ui.rb
@@ -5,4 +5,12 @@ module UI
     right:    NSTextAlignmentRight,
     justify:  NSTextAlignmentJustified
   }
+
+  def self.resource_str(name)
+    if md = name.match(/(.*)\.(.*)$/)
+      if path = NSBundle.mainBundle.pathForResource(md[1], ofType:md[2])
+        File.read(path)
+      end
+    end
+  end
 end
