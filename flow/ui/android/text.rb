dd66f3eef4135414a0ac6bb78abd17ab20dea4a1
Laurent Sansonetti <lrz@hipbyte.com>
2017-03-28 18:17:46 +0200

    add links support to UI::Text

--- flow/ui/android/text.rb
+++ flow/ui/android/text.rb
@@ -1,7 +1,18 @@
+class FlowUITextViewSpan < Android::Text::Style::ClickableSpan
+  def initialize(view, link)
+    @view, @link = view, link
+  end
+
+  def onClick(view)
+    @view.trigger :link, @link
+  end
+end
+
 module UI
   class Text < UI::View
     include UI::SharedText
     def text_proxy; proxy; @text_proxy; end
+    include Eventable
 
     def editable?
       @editable
@@ -12,6 +23,25 @@ module UI
         @editable = flag
         @proxy = nil
       end
+    end
+
+    attr_accessor :link_color
+
+    def links=(links)
+      ss = Android::Text::SpannableString.new(self.text)
+
+      links.each do |range, link|
+        span = FlowUITextViewSpan.new(self, link)
+        ss.setSpan(span, range.begin, range.end, Android::Text::Spanned::SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        if @link_color
+          fspan = Android::Text::Style::ForegroundColorSpan.new(UI.Color(@link_color).proxy)
+          ss.setSpan(fspan, range.begin, range.end, Android::Text::Spanned::SPAN_INCLUSIVE_INCLUSIVE)
+        end
+      end
+
+      @text_proxy.text = ss
+      @text_proxy.movementMethod = Android::Text::Method::LinkMovementMethod.getInstance
     end
 
     def proxy
--- flow/ui/cocoa/text.rb
+++ flow/ui/cocoa/text.rb
@@ -1,6 +1,14 @@
+class FlowUITextView < UITextView
+  # Flow text views are not selectable at the moment.
+  def canBecomeFirstResponder
+    false
+  end
+end
+
 module UI
   class Text < View
     include UI::SharedText
+    include Eventable
 
     def editable=(flag)
       proxy.editable = flag
@@ -10,8 +18,50 @@ module UI
       proxy.editable
     end
 
+    attr_reader :link_color
+
+    def link_color=(color)
+      if @link_color != color
+        @link_color = color
+        proxy.linkTextAttributes = { NSForegroundColorAttributeName => UI.Color(color).proxy }
+      end
+    end
+
+    def links=(links)
+      at = proxy.attributedText.mutableCopy
+
+      links.each do |range, link|
+        range = [range.begin, range.end - range.begin]
+        at.addAttribute(NSLinkAttributeName, value:_add_link(link), range:range)
+        at.addAttribute(NSUnderlineStyleAttributeName, value:1, range:range)
+      end
+
+      proxy.attributedText = at
+      proxy.dataDetectorTypes = UIDataDetectorTypeLink
+    end
+
+    def _add_link(value)
+      @links ||= {}
+      link = "link#{@links.size}"
+      @links[link] = value
+      NSURL.URLWithString(link + '://')
+    end
+
+    def textView(view, shouldInteractWithURL:url, inRange:range)
+      if @links and value = @links[url.scheme]
+        trigger :link, value
+        false 
+      else
+        true
+      end
+    end
+
     def proxy
-      @proxy ||= UITextView.alloc.init
+      @proxy ||= begin
+        view = FlowUITextView.alloc.init
+        view.delegate = self
+        view
+      end
     end
   end
 end
