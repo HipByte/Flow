54d9ae11ac455924c4ad5a50cfea732bdf891aeb
Khalil Gharbaoui <khalil@adcombi.com>
2020-09-19 04:49:44 +0200

    Allow app.api_version overwrite by environment var

--- lib/android.rb
+++ lib/android.rb
@@ -5,7 +5,7 @@ require 'motion/project/template/android'
 require 'motion-gradle'
 
 Motion::Project::App.setup do |app|
-  app.api_version = '23' unless Motion::Project::Config.starter?
+  app.api_version = ENV['ANDROID_APP_API_VERSION'] ||= '23' unless Motion::Project::Config.starter?
   app.build_dir = 'build/android'
   app.assets_dirs << 'resources'
   app.resources_dirs = []
